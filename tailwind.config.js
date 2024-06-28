d15494e24e8fa073ac0f9498f005cf5675e28114
DTS <officialdts11@gmail.com>
2024-06-28 15:12:47 +0530

    extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary: '#E84C3D', // logo red
+				primary_hover: '#D94335',
+				secondary: '#34AADC', // blue
+				secondary_hover: '#2B91C5',
+				tertiary: '#FFB84D', // yellow
+				tertiary_hover: '#E6A441'
+			}
+		}
 	},
 	plugins: []
 };
